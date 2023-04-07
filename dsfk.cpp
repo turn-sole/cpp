99bf6c1b7cd4f615c3f64ee87e4a1d3403817aba
turn-sole <113235914+turn-sole@users.noreply.github.com>
2023-04-07 22:23:25 +0800

    Add files via upload

---
+++ dsfk.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+//int main()
+//{
+//	cout << "name" << endl;
+//	cout << "address" << endl;
+//	return 0;
+//}
+//int main()
+//{
+//	int distance;
+//	cin >> distance;
+//	cout << "enter a" << distance << "distance=" << distance * 220 << "code" << endl;
+//	return 0;
+//}
+//void print1()
+//{
+//	cout << "Three blind mice" << endl;
+//	cout<<"Three blind mice"
+//}
+//int main()
+//{
+//	print1();
+//	print2();
+//	return 0;
+//}
+//#define ZERO 0
+//#include<climits>
+//int main()
+//{
+//	short sam = SHRT_MAX;
+//	unsigned short sue = sam;
+//	cout << "Sam has" << sam << "dollars and Sue has" << sue;
+//	cout << "dollars deposited." << endl
+//		<< "Add $1 to each accout." << endl << "Now";
+//	sam = sam + 1;
+//	sue = sue + 1;
+//	cout << "Sam has" << sam << "dollars and Sue has" << sue;
+//	cout << "dollars deposited.\nPoor Sam!" << endl;
+//	sam = ZERO;
+//	sue = ZERO;
+//	cout << "Sam has" << sam << "dollars and Sue has" << sue;
+//	cout << "dollars deposited." << endl;
+//	cout << "Take $1 from each accout." << endl << "Now";
+//	sam = sam - 1;
+//	sue = sue - 1;
+//	cout << "Sam has" << sam << "dollars and Sue has" << sue;
+//	cout << "dollars deposited." << endl << "Lucky Sue!" << endl;
+//}
+//int main()
+//{
+//	int chest = 42;
+//	int waist = 0x42;
+//	int inseam = 042;
+//	cout << "Mosieur cuts a striking figure!\n";
+//	cout << "chest=" << chest << "(42 in decimal)\n";
+//	cout << "waist=" << waist << "(0x42 in hex)\n";
+//	cout << "inseam=" << inseam << "(042 in octal)\n";
+//	return 0;
+//}
