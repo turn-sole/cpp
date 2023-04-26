2d27275ecc73cd5b57e64ce5d90346e03d057e21
turn-sole <113235914+turn-sole@users.noreply.github.com>
2023-04-26 22:59:19 +0800

    Add files via upload

---
+++ fdsja.cpp
@@ -0,0 +1,23 @@
+//class Data
+//{
+//public:
+//	void init(int year)
+//	{
+//		year = year;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//int top
+//{
+//	return a[top - 1];
+//}
+//int main()
+//{
+//	stack st1;
+//	stack st2;
+//	cout << sizeof(st1) << endl;
+//	return 0;
+//}
