c642eb53595ca19fe6ee12b3ff1d5932ce60a72a
turn-sole <113235914+turn-sole@users.noreply.github.com>
2023-04-27 22:41:48 +0800

    Add files via upload

---
+++ jkF.cpp
@@ -0,0 +1,111 @@
+#include<iostream>
+using namespace std;
+//class A
+//{
+//public:
+//	void PrintA()
+//	{
+//		cout << _a << endl;
+//	}
+//private:
+//	char _a;
+//};
+//
+//int main()
+//{
+//	A aa1;
+//	A aa2;
+//	A aa3;
+//	cout << sizeof(A) << endl;
+//	cout << sizeof(aa1) << endl;
+//	aa1._a = 1;
+//	aa1.PrintA();
+//	return 0;
+//}
+//
+//class A1 {
+//public:
+//	void f1(){}
+//private:
+//	int _a;
+//	char _ch;
+//};
+//
+//class A2 {
+//public:
+//	void f2(){}
+//};
+//
+//class A3
+//{};
+//
+//int main()
+//{
+//	cout << sizeof(A2) << endl;
+//	cout << sizeof(A3) << endl;
+//	A2 aa1;
+//	A3 aa2;
+//	cout << &aa1 << endl;
+//	cout << &aa2 << endl;
+//	return 0;
+//}
+//class Date
+//{
+//public:
+//	void Init(int year, int month, int day)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//	void Print()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//private:
+//	int _year;     // Äê
+//	int _month;    // ÔÂ
+//	int _day;      // ÈÕ
+//};
+//int main()
+//{
+//	Date d1, d2;
+//	d1.Init(2022, 1, 11);
+//	d2.Init(2022, 1, 12);
+//	d1.Print();
+//	d2.Print();
+//	return 0;
+//}
+//class A
+//{
+//public:
+//	void Print()
+//	{
+//		cout << "Print()" << endl;
+//	}
+//private:
+//	int _a;
+//};
+//int main()
+//{
+//	A* p = nullptr;
+//	p->Print();
+//	return 0;
+//}
+//
+//class A
+//{
+//public:
+//	void PrintA()
+//	{
+//		cout << _a << endl;
+//	}
+//private:
+//	int _a;
+//};
+//int main()
+//{
+//	A* p = nullptr;
+//	p->PrintA();
+//	return 0;
+//}
