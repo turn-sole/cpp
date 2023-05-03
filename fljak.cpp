972c8ab19d0d500daabe475daa35341bf7f96e2f
turn-sole <113235914+turn-sole@users.noreply.github.com>
2023-05-03 21:59:34 +0800

    Add files via upload

---
+++ fljak.cpp
@@ -0,0 +1,132 @@
+#include<iostream>
+using namespace std;
+
+//class Data
+//{
+//public:
+//	Data(int year = 1900, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//	Data(const Data& d)
+//	{
+//		cout << "Data(Data&d)"<<endl;
+//		_year = d._year;
+//		_month = d._month;
+//		_year = d._day;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//void func(Data d)
+//{
+//
+//}
+//
+//void func(int i)
+//{
+//
+//}
+//int main()
+//{
+//	Data d1(2023, 4, 25);
+//	Data d2(d1);
+//	func(d1);
+//	func(10);
+//}
+
+//void func(Data& d)
+//{
+//
+//}
+//
+//void func(Stack st)
+//{
+//
+//}
+//
+//Stack func()
+//{
+//	Stack st;
+//	return st;
+//}
+//
+//int main()
+//{
+//	Data d1;
+//	func(d1);
+//
+//	Stack st1;
+//	func(st1);
+//
+//	func();
+//	return 0;
+//}
+
+//bool operator<(const Date& x1, const Date& x2)
+//{
+//	if (x1._year < x2._year)
+//	{
+//		return true;
+//	}
+//	else if (x1._year == x2._year && x1._month < x2._month)
+//	{
+//		return true;
+//	}
+//	else if (x1._year == x2._year && x1._month == x2._month && x1._day < x2._day)
+//	{
+//		return true;
+//	}
+//
+//	return false;
+//}
+//
+//bool operator>(const Date& x1, const Date& x2)
+//{
+//	if (x1._year > x2._year)
+//	{
+//		return true;
+//	}
+//	else if (x1._year == x2._year && x1._month > x2._month)
+//	{
+//		return true;
+//	}
+//	else if (x1._year == x2._year && x1._month == x2._month && x1._day > x2._day)
+//	{
+//		return true;
+//	}
+//
+//	return false;
+//}
+//
+//int main()
+//{
+//	Date d1(2025, 4, 25);
+//	Date d2(2023, 5, 25);
+//
+//	/*cout << Func1(d1, d2) << endl;
+//	cout << Func2(d1, d2) << endl;*/
+//
+//	// 为什么内置类型可以直接比较，自定义类型不可以直接比较？
+//	cout << (d1 < d2) << endl;
+//	cout << (operator<(d1, d2)) << endl;
+//
+//	cout << (d1 > d2) << endl;
+//	cout << (operator>(d1, d2)) << endl;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	Date d1(2023, 4, 26);
+//	Date d2(2023, 6, 21);
+//
+//	d1 - d2;
+//	d1 + d2;
+//}
+
